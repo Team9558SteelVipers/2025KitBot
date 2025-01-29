52eb0db30eb51d77cd6e2c450d5c9d14af0ef1fc
Julian <juliankhoury586@gmail.com>
2025-01-29 18:52:18 -0500

    .

--- src/main/java/frc/robot/Subsystems/Commands/Constants.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.Subsystems.Commands;
-
-public class Constants {
- 
-    public static final int BackRightMotor = 0;
-    public static final int BackLeftMotor = 0;
-    public static final int FrontRightMotor = 0;
-    public static final int FrontLeftMotor = 0;
-}
