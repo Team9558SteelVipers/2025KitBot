150827753973843201e6252763ddb58f94aa1912
ArshillC <148399184+ArshillC@users.noreply.github.com>
2025-01-29 18:41:39 -0500

    Tank Drive Code

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public final class Constants {
+  //public static class OperatorConstants {
+    // Controllers
+    public static final int kDriverControllerPort = 0;
+
+    // Drive Motor
+    public static final int FLDriveMotorPort = 0;
+    public static final int FRDriveMotorPort = 0;
+    public static final int BLDriveMotorPort = 0;
+    public static final int BRDriveMotorPort = 0;
+
+    // Coral Shooting Motors
+    public static final int coralMotorPort = 2;
+
+  //}
+}
